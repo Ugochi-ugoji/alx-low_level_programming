b3a071c38a1a4f3a669c9f4101b9d178e33a5138
Ugochi Chimerem Ugoji <ugoochy@gmail.com>
2022-09-09 23:09:00 -0700

    if else variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,8 +10,9 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
-	
+
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	printf("%d is positive\n", n);
@@ -19,6 +20,7 @@ int main(void)
 	printf("%d is zero\n", n);
 	else
 	printf("%d is negative\n", n);
+
 	return (0);
 }
 
