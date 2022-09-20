4409cd7d6363b601964beff73fdf0368119cff75
Ugochi Chimerem Ugoji <ugoochy@gmail.com>
2022-09-19 20:40:03 -0700

    pointers arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * reset_to_98 - Resets the value fo a variable to 98
- * pointer dereference for changing the value of the variable
+ * @n: pointer dereference for changing the value of the variable
  * Return 0 success.
  */
 
