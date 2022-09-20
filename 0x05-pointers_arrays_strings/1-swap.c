6a4fdf736e9bd96641f14b09e4484dace0773982
Ugochi Chimerem Ugoji <ugoochy@gmail.com>
2022-09-19 21:18:56 -0700

    pointers arrays and strings

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,21 +1,17 @@
 #include "main.h"
-
 #include <stdio.h>
 
-
-
 /**
- * @a: pointer changing the value of variable a
- * @b:pointer changing the value of variable b
- * @c: pointer changing the value of c 
- * swap_int: function swaps he value of integers
+ * @a: a pointer changing the value of variable a
+ * @b: a pointer changing the value of variable b
+ * @c: a pointer changing the value of c 
+ * swap_int: a function that swaps he value of integers
  * Return: Always 0
  */
 
 void swap_int(int *a, int *b)
 {
-
-		int c = *a;
-		*a = *b;
-		*b = c;
+	int c = *a;
+	*a = *b;
+	*b = c;
 }
