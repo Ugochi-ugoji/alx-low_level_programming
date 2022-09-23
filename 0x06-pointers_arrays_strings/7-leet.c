6827c20a71f4ec2722469d67ea8756bcdadcab01
Ugochi Chimerem Ugoji <ugoochy@gmail.com>
2022-09-22 20:14:52 -0700

    pointers arrays strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -16,11 +16,12 @@ char *leet(char *s)
 {
 	int stringCount, leetCount;
 	char leetLetters[] = "aAeEoOtTlL";
-	char leetNums[] = "4433007711"; 
-	/*  scan through string */
+	char leetNums[] = "4433007711";
 
+/*  scan through string */
 	stringCount = 0;
-	while (s[stringCount] != '\0')/* check whether leetLetter is found */
+	while (s[stringCount] != '\0')
+/* check whether leetLetter is found */
 	{
 		leetCount = 0;
 		while (leetCount < 10)
